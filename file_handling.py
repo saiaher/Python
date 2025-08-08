b2a5e15b9ed965dce9d36286e181ad7facc2f624
saiaher <ahersai1414@gmail.com>
2025-08-08 11:00:19 +0530

    file_handing

---
+++ file_handling.py
@@ -0,0 +1,21 @@
+
+# try:
+#     print(5 / 0)
+# except:
+#     print("ssi")
+       
+# else:
+#     print("Result:")
+
+
+subjects=["Americans ","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+try:
+    for i in subjects:
+        for j in verbs:
+            for k in objects:
+
+                print(f"{i} {j} {k}")
+except:
+    print ("that code have mistake then excpet block run")               
