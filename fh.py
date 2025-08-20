529d5a54ca94be3d2f6ce20a4c292d1b1bf7f74e
saiaher <ahersai1414@gmail.com>
2025-08-20 13:18:15 +0530

    adding_problems

--- fh.py
+++ fh.py
@@ -1,1 +1,7 @@
-fg
+sai=1,2,3,4
+x=sai[0]
+for i in sai[1:]:
+    print(i)
+    if i < x:
+        x=i
+return x  
--- python_problems.py
+++ python_problems.py
@@ -24,3 +24,49 @@ def palindeome(x):
         print("it's not a palindrome")
 palindeome("ram ram")
 palindeome("121121")
+
+# count words
+ 
+sentence=input("enter your sentence:")
+
+print(len(sentence.split())) 
+
+# Find the Smallest Number
+
+def find_smallest_number(numbers):
+    if not numbers:
+        return None  
+    smallest = numbers[0]
+    for number in numbers[1:]:
+        if number < smallest:
+            smallest = number
+    return smallest
+
+my_list = [1,2,3,4,5,6,0]
+smallest_number = find_smallest_number(my_list)
+print("The smallest number in the list is:", smallest_number)
+
+# sum of digit:S
+
+def sum_digit(x):
+    add=0
+    for i in x:
+        add+=i
+    return add    
+        
+        
+
+s1=sum_digit([1,2,3,4,5,6])
+print(s1)
+
+
+# find all multiples
+multiple=[]
+for i in range(1,51):
+    if i%3==0:
+        multiple.append(i)
+    elif i%5==0:
+         multiple.append(i)    
+print(multiple)    
+
+
