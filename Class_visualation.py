a76e5a0af18ecd2b589aed14c508f02dd95bf74c
saiaher <ahersai1414@gmail.com>
2025-09-10 08:58:34 +0530

    matblotlib

--- Class_visualation.py
+++ Class_visualation.py
@@ -16,20 +16,28 @@ ts = pd. Series (randn(1000), index = pd.date_range('2019-06-07', periods = 1000
 # print(df1.plot(figsize=(50,20)))
 # print(plt.plot(ts))
 # print(plt.show())
-
-# s1 = sns.load_dataset(('iris'))# print(s1.head())
+s1 = sns.load_dataset(('iris'))# print(s1.head())
 # s1.plot()
 # print(plt.show ())
 
-ts.plot(figsize=(1000,5))
-plt.show()
+# ts.plot(figsize=(1000,5))
+# plt.show()
 
 # s1.plot()
 # plt.show()
 
 
-ts.plot(kind='bar',figsize=(1000,5))
-plt.show()
+# df.plot(kind='bar',figsize=(1000,5))
+# plt.show()
+
+# x=ts.plot(kind ='barh', figsize=(200,80))
+# print(x)
+# plt.show()
+
+print(s1)
+
+s2 = s1.drop(['species'], axis = 1)
+print(s2.head())
 
 
 
